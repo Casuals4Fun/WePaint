173f639cb253041410c033cc97148e605d0967c9
Shubham Lal <lal.shubham027@gmail.com>
2023-09-18 00:42:56 +0530

    moved all files & folders to client

---
+++ client/types/typing.d.ts
@@ -0,0 +1,12 @@
+type Draw = {
+    ctx: CanvasRenderingContext2D
+    currPoint: Point
+    prevPoint: Point | null
+};
+
+type Point = { x: number, y: number };
+
+type ThemeState = {
+    theme: string,
+    setTheme: (theme: string) => void
+}
