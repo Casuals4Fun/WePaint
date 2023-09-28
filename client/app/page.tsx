41fffce8f38e484685ed980200900b301420e12f
Shubham Lal <lal.shubham027@gmail.com>
2023-09-28 22:15:08 +0530

    client: wip window height

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,12 +4,20 @@ import Navbar from '@/components/Navbar';
 import DrawCanvas from '@/components/DrawCanvas';
 import Footer from '@/components/Footer';
 import { useThemeStore } from '@/store';
+import useWindowHeight from '@/utils/useWindowHeight';
 
 const Home = () => {
   const { theme } = useThemeStore();
+  const { height, isReady } = useWindowHeight();
 
   return (
-    <div className={`relative w-screen min-h-screen flex flex-col items-center justify-between ${theme === "light" ? "bg-white" : "bg-black"}`}>
+    <div className={`relative w-screen flex flex-col items-center justify-between ${theme === "light" ? "bg-white" : "bg-black"}`}
+      style={{
+        minHeight: `${height}px`,
+        opacity: isReady ? 1 : 0,
+        transition: 'opacity 0.5s linear'
+      }}
+    >
       <Navbar />
       <DrawCanvas />
       <Footer />
