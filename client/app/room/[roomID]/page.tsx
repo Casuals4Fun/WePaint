75d2851a32f93b52ff703eb16fa996c2550bea5f
Shubham Lal <lal.shubham027@gmail.com>
2023-10-01 12:55:06 +0530

    client: wip Invite modal

--- client/app/room/[roomID]/page.tsx
+++ client/app/room/[roomID]/page.tsx
@@ -1,18 +1,24 @@
 'use client';
 
 import { useParams } from 'next/navigation';
-import { useThemeStore } from '@/store';
+import { useInviteStore, useThemeStore } from '@/store';
 import useWindowHeight from '@/utils/useWindowHeight';
 import Navbar from '@/components/Navbar';
 import RoomCanvas from '@/components/RoomCanvas';
 import Footer from '@/components/Footer';
+import { useEffect } from 'react';
 
 const InviteRoom = () => {
     const params = useParams();
     const roomID = params.roomID;
 
     const { theme } = useThemeStore();
+    const { setPreference } = useInviteStore();
     const { height, canvasHeight, isReady } = useWindowHeight();
+
+    useEffect(() => {
+        if (roomID) setPreference("Share");
+    }, [roomID]);
 
     return (
         <div className={`overflow-y-hidden relative w-screen flex flex-col items-center justify-between ${theme === "light" ? "bg-white" : "bg-black"}`}
--- client/components/Invite.tsx
+++ client/components/Invite.tsx
@@ -6,6 +6,8 @@ import { IoIosArrowBack } from 'react-icons/io';
 import { AiOutlineClose } from 'react-icons/ai';
 import { GrAdd } from 'react-icons/gr';
 import { GoPeople } from 'react-icons/go';
+import { BsFillClipboardFill } from 'react-icons/bs';
+import { useParams } from 'next/navigation';
 
 const Invite = () => {
     const { invite, setInvite, preference, setPreference } = useInviteStore();
@@ -18,8 +20,8 @@ const Invite = () => {
     return (
         <div className="fixed inset-0 flex items-center justify-center z-50">
             <div className="bg-gray-100 opacity-70 fixed inset-0 z-40"></div>
-            <div className="bg-white w-[95%] md:w-[500px] min-h-[214px] mx-auto rounded-lg shadow-lg overflow-hidden z-50 relative">
-                {preference !== "" && (
+            <div className="bg-white w-[95%] md:w-[500px] h-[214px] mx-auto rounded-lg shadow-lg overflow-hidden z-50 relative">
+                {(preference !== "Share" && preference !== "") && (
                     <button
                         className='absolute left-0 top-0 w-[30px] h-[30px] bg-gray-100 hover:bg-black text-black hover:text-white duration-200 flex items-center justify-center'
                         onClick={() => setPreference("")}
@@ -35,37 +37,19 @@ const Invite = () => {
                         <AiOutlineClose />
                     </button>
                 )}
-                <div className="p-5">
+                <div className="p-5 h-full">
                     {preference === "" ? (
                         <PreferenceSelector />
                     ) : preference === "Create" ? (
                         <CreateRoom />
                     ) : preference === "Join" ? (
                         <JoinRoom />
+                    ) : preference === "Share" ? (
+                        <ShareRoom />
                     ) : null}
                 </div>
             </div>
         </div>
-    )
-};
-
-const CreateRoom = () => {
-    return (
-        <>
-            <p className='text-[20px] text-center mb-4'>
-                Create Room
-            </p>
-        </>
-    )
-};
-
-const JoinRoom = () => {
-    return (
-        <>
-            <p className='text-[20px] text-center mb-4'>
-                Join Room
-            </p>
-        </>
     )
 };
 
@@ -104,5 +88,57 @@ const PreferenceSelector = () => {
         </>
     )
 };
+
+const CreateRoom = () => {
+    return (
+        <>
+            <p className='text-[20px] text-center mb-4'>
+                Create Room
+            </p>
+        </>
+    )
+};
+
+const JoinRoom = () => {
+    return (
+        <>
+            <p className='text-[20px] text-center mb-4'>
+                Join Room
+            </p>
+        </>
+    )
+};
+
+const ShareRoom = () => {
+    const params = useParams();
+    const roomID = params.roomID;
+
+    return (
+        <div className='h-full flex flex-col justify-between'>
+            <p className='text-[20px] text-center'>
+                Share Invite
+            </p>
+            <div className='w-full flex border border-gray-300 rounded-md pl-2 overflow-hidden'>
+                <div className='w-[90%] text-ellipsis overflow-hidden border-r border-gray-300 py-2'>
+                    {`${process.env.NEXT_PUBLIC_FRONTEND_URL}/room/${roomID}`}
+                </div>
+                <div
+                    title='Copy URL'
+                    className='w-[10%] h-full flex items-center justify-center bg-black py-2 text-white duration-200 cursor-pointer group'
+                >
+                    <BsFillClipboardFill />
+                </div>
+            </div>
+            <div className='flex justify-between items-center'>
+                <button className='hover:underline'>
+                    Create Room
+                </button>
+                <button className='hover:underline'>
+                    Join Room
+                </button>
+            </div>
+        </div>
+    )
+}
 
 export default Invite
