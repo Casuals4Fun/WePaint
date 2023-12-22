29636000ca4b270a36c33208dd160bf025672b4d
Shubham Lal <im.shubhamlal@gmail.com>
2023-12-22 13:12:04 +0530

    fix deploy action

--- client/server.js
+++ client/server.js
@@ -1,27 +1,34 @@
-const express = require("express");
+const { createServer } = require("http");
+const { parse } = require("url");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
+const hostname = "localhost";
+const port = process.env.PORT || 3000;
+
+const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express();
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
 
-  // Define custom routes here, if needed
-  server.get("/room/*", (req, res) => {
-    return app.render(req, res, "/room/*", req.query);
-  });
-
-  server.get("*", (req, res) => {
-    return handle(req, res);
-  });
-
-  const port = process.env.PORT || 3000;
-
-  server.listen(port, (err) => {
+      if (pathname === "/a") {
+        await app.render(req, res, "/a", query);
+      } else if (pathname === "/b") {
+        await app.render(req, res, "/b", query);
+      } else {
+        await handle(req, res, parsedUrl);
+      }
+    } catch (err) {
+      console.error("Error occurred handling", req.url, err);
+      res.statusCode = 500;
+      res.end("internal server error");
+    }
+  }).listen(port, (err) => {
     if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+    console.log(`> Ready on http://${hostname}:${port}`);
   });
 });
-
